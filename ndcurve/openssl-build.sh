a144dd2b440263e3c3b6692aafb99d22d7402102
Muneer <elendilm@gmail.com>
2026-07-03 04:09:42 +0530

    minors

--- ndcurve/openssl-build.sh
+++ ndcurve/openssl-build.sh
@@ -9,8 +9,13 @@
 
  ANDROID_NDK=$ANDROID_NDK_ROOT
  ANDROID_API_LEVEL=$ANDROID_SDK_API_LEVEL
- SSL_SUFFIX=_3
+ SSL_SUFFIX=_4
 
+ # Android 16 KB Page Alignment. NDK 27.2 by default doesn't do this. This may be removed if we switch to NDK 28 as it does compile by default.
+#  export LDFLAGS="-Wl,-z,max-page-size=16384"
+
+#  OPEN_SSL_VERSION=3.1.0
+ OPEN_SSL_VERSION=4.0.1
  # ----------------------------------------------------------------------------
 
  export ANDROID_NDK_ROOT=$ANDROID_NDK
@@ -23,7 +28,7 @@
 function build() {
  rm -rf "latest/$ARCH" && mkdir -p "latest/$ARCH"
  rm -rf build && mkdir -p build && cd build
- ../openssl-openssl-3.1.0/Configure shared android-$ARCH -D__ANDROID_API__=$ANDROID_API_LEVEL
+ ../openssl-openssl-${OPEN_SSL_VERSION}/Configure shared android-$ARCH -D__ANDROID_API__=$ANDROID_API_LEVEL
  make -j$(nproc) SHLIB_VERSION_NUMBER= SHLIB_EXT=$SSL_SUFFIX.so build_libs
  cp libcrypto.a "../latest/$ARCH/libcrypto$SSL_SUFFIX.a"
  cp libssl.a "../latest/$ARCH/libssl$SSL_SUFFIX.a"
@@ -35,7 +40,7 @@ function build() {
 function build_noasm() {
  rm -rf "latest/no-asm/$ARCH" && mkdir -p "latest/no-asm/$ARCH"
  rm -rf build && mkdir -p build && cd build
- ../openssl-openssl-3.1.0/Configure shared android-$ARCH -D__ANDROID_API__=$ANDROID_API_LEVEL no-asm
+ ../openssl-openssl-${OPEN_SSL_VERSION}/Configure shared android-$ARCH -D__ANDROID_API__=$ANDROID_API_LEVEL no-asm
  make -j$(nproc) SHLIB_VERSION_NUMBER= SHLIB_EXT=$SSL_SUFFIX.so build_libs
  cp libcrypto.a "../latest/no-asm/$ARCH/libcrypto$SSL_SUFFIX.a"
  cp libssl.a "../latest/no-asm/$ARCH/libssl$SSL_SUFFIX.a"
